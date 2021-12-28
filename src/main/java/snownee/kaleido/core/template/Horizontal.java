674cac7c2dae038f925c3887d79a7b7a418cc160
Snownee <mjy523@126.com>
2021-12-28 17:08:30 +0800

    fix: mismatched horizontal block serialization

--- src/main/java/snownee/kaleido/core/template/Horizontal.java
+++ src/main/java/snownee/kaleido/core/template/Horizontal.java
@@ -3,6 +3,7 @@ package snownee.kaleido.core.template;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.util.Direction;
 
 public class Horizontal extends KaleidoTemplate {
 
@@ -13,6 +14,11 @@ public class Horizontal extends KaleidoTemplate {
 	@Override
 	protected int _toMeta(BlockState state) {
 		return state.getValue(HorizontalBlock.FACING).get2DDataValue();
+	}
+
+	@Override
+	protected BlockState _fromMeta(int meta) {
+		return stateContainer.any().setValue(HorizontalBlock.FACING, Direction.from2DDataValue(meta));
 	}
 
 }
